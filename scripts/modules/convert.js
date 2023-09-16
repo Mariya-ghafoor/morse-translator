ca9a36c71b4793e4589b95b173c413f6c25c74b1
Mariya Kashif <mariyakashif@Mariyas-MacBook-Air.local>
2023-09-16 16:45:04 +1000

    adds translator

---
+++ scripts/modules/convert.js
@@ -0,0 +1,65 @@
+// A to Z in Morse Code
+const morseCode = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
+
+//Function to convert to Morse Code
+
+export const convertToMorse = (userInput) => {
+  let userInputArr = userInput.toUpperCase().split("");
+  console.log({ userInputArr });
+
+  const morseTranslation = userInputArr.map((char) => {
+    if (char === " ") return " / ";
+    else {
+      return morseCode[char] + " ";
+    }
+  });
+  console.log({ morseTranslation });
+  //console.log(morseTranslation.join(""));
+  return morseTranslation.join("");
+};
+
+//Function to convert to English
+
+export const convertToEnglish = (userInput) => {
+  console.log("im converting to english", userInput);
+  const userInputArr = userInput.split(" ");
+  console.log({ userInput });
+
+  const englishTranslation = userInputArr.map((char) => {
+    console.log({ char });
+    if (char === "/") return " ";
+    const morseCodeKeys = Object.keys(morseCode);
+    const englishChar = morseCodeKeys.find((key) => morseCode[key] === char);
+    return englishChar;
+  });
+
+  console.log(englishTranslation);
+
+  return englishTranslation.join("");
+};
---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+import { convertToEnglish, convertToMorse } from "./modules/convert.js";
+
+const form = document.querySelector("form");
+const translationPara = document.querySelector("#translationPara");
+
+//Validate Input
+// Remove trailing spaces
+// only accept alphabets and space
+// or if . and -- then no other character should be entered except space and /
+// handle cases where morse code may be wrong and cannot be translated
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userInput = document.getElementById("userInput").value.trim();
+  console.log(userInput);
+  let convertedCode = "";
+
+  //Input Validation
+
+  //Should either include only alphabets and spaces (for English) or dots,dashes, spaces and forward slash for morse code
+  if (/^[a-zA-Z ]+$/.test(userInput)) {
+    convertedCode = convertToMorse(userInput);
+  } else if (/^[\.\-\/\s]+$/.test(userInput)) {
+    convertedCode = convertToEnglish(userInput);
+    console.log({ convertedCode });
+    if (convertedCode.length === 0)
+      convertedCode =
+        "Hmm..that may look like Morse code but its not. Try again.";
+  } else {
+    convertedCode = "Please enter either English or Morse code";
+  }
+
+  translationPara.innerText = convertedCode;
+});
